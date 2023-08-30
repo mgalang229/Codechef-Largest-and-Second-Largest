7e5d47291be1e8d161262bebda2975a40483db40
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-08-30 22:30:06 +0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+//change class to 'Main'
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner fs = new Scanner(System.in);
+		int T = 1;
+		T = fs.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int n = fs.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = fs.nextInt();
+			}
+			shuffleSort(a);
+			int ans = a[n-1];
+			for (int i = n - 2; i >= 0; i--) {
+				if (a[i] != a[n-1]) {
+					ans += a[i];
+					break;
+				}
+			}
+			System.out.println(ans);
+		}
+		fs.close();
+	}
+	
+	static final Random rnd = new Random();
+	static void shuffleSort(int[] a) { //change this
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int newInd = rnd.nextInt(n);
+			int temp = a[newInd]; //change this
+			a[newInd] = a[i];
+			a[i] = temp;
+		}
+		Arrays.sort(a);
+	}
+}
